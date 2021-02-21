b4112ef53c46f1dfa308504139309f019031a101
thewiso <tschoenebaum@gmail.com>
2021-02-21 16:18:41 +0100

    fixing startWLAN script

--- setup/startWLAN.sh
+++ setup/startWLAN.sh
@@ -6,7 +6,7 @@ if [ "$1" = "check" ]
 then
 	echo "Check if connected to home network..."
 	sleep 15
-	GREP_COUNT=$(iwconfig wlan0 | grep -c "CHANGE_TO_YOUR_HOME_SSID")
+	GREP_COUNT=$(iwconfig wlan0 | grep -c "$HOME_NETWORK_SSID")
 	if [ "$GREP_COUNT" = "0" ]
 	then
 		echo "Not connected to home network"
@@ -21,10 +21,10 @@ fi
 
 if [ "$START_WLAN" = "true" ]
 then
-	echo "Starting own WLAN"
+	echo "Starting own WLAN..."
 	sudo killall wpa_supplicant
 	sudo ifconfig wlan0 down
 	sudo ifconfig wlan0 192.168.0.1 netmask 255.255.255.0 up
 	sudo service dnsmasq start
-	sudo hostapd setup/hostapd.conf &
+	sudo hostapd /home/pi/led-box/setup/hostapd.conf &
 fi
