b2ea5d032f243dd301fcfc1b43bffb21203f0469
thewiso <tschoenebaum@gmail.com>
2021-02-21 09:59:29 +0100

    adding swagger-ui dependency, fixing setup script

--- setup/installRuntimeDependencies.sh
+++ setup/installRuntimeDependencies.sh
@@ -1,14 +1,10 @@
 echo "Installing general dependencies..."
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y git python3 python3-pip nginx
 pip3 install virtualenv
 
 echo "Installing python dependencies..."
 python3 -m virtualenv led-box-backend/env
-source led-box-backend/env/bin/activate
-pip3 install -r led-box-backend/requirements.txt
-#optional:
-#pip3 install connexion[swagger-ui]
-deactivate
+python3 -m pip install -r led-box-backend/requirements.txt
 
 echo "Finished successfully!"
