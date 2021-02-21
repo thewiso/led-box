c6d97836e230ede4edf2598c629295d9e008f0b5
thewiso <tschoenebaum@gmail.com>
2021-02-21 16:53:33 +0100

    adding captive portal for ios and android

--- setup/configureStandaloneWLAN.sh
+++ setup/configureStandaloneWLAN.sh
@@ -3,3 +3,6 @@
 sudo apt-get install -y hostapd dnsmasq
 sudo systemctl disable dnsmasq
 sudo bash -c 'cat setup/dnsmasq.conf >> /etc/dnsmasq.conf'
+
+sudo cp setup/captive_portal_nginx.conf /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/captive_portal_nginx.conf /etc/nginx/sites-enabled/
