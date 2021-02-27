be72de27d7f97d4281e3d1911348b55e885ba674
thewiso <tschoenebaum@gmail.com>
2021-02-27 12:05:37 +0100

    adding example patterns

--- led-box-backend/db/example_patterns.sql
+++ led-box-backend/db/example_patterns.sql
@@ -1,76 +1,222 @@
 INSERT INTO LED_PATTERN (JSON) VALUES ('{
-	"name": "none_animation",
-	"colors": [
-		{
-			"r": 255,
-			"g": 0,
-			"b": 0
-		},
-		{
-			"r": 0,
-			"g": 255,
-			"b": 0
-		},
-		{
-			"r": 0,
-			"g": 0,
-			"b": 255
-		}
-	],
-	"repitionFactor": 0,
-	"colorGradientLengthFactor": 0.3,
-	"patternType": "LEDPattern"
-}');
+		"chaseGradientLengthFactor": 0.26364481723023114,
+		"chaseLengthFactor": 0.1518038708613425,
+		"chaseSpeed": 14.201110233363739,
+		"colorGradientLengthFactor": 0.020774838980948375,
+		"colors": [
+			{
+				"b": 112,
+				"g": 21,
+				"r": 22
+			},
+			{
+				"b": 255,
+				"g": 255,
+				"r": 255
+			},
+			{
+				"b": 11,
+				"g": 3,
+				"r": 70
+			}
+		],
+		"id": 4,
+		"name": "Indipendence Day",
+		"patternType": "ChaseLEDPattern",
+		"repitionFactor": 0.849949579039041
+    }');
 
 INSERT INTO LED_PATTERN (JSON) VALUES ('{
-	"name": "blink_animation",
-	"colors": [
-		{
-			"r": 255,
-			"g": 0,
-			"b": 0
-		},
-		{
-			"r": 0,
-			"g": 255,
-			"b": 0
-		},
-		{
-			"r": 0,
-			"g": 0,
-			"b": 255
-		}
-	],
-	"repitionFactor": 0,
-	"colorGradientLengthFactor": 0.3,
-	"patternType": "BlinkLEDPattern",
-	"blinkSpeed": 0.1,
-	"blinkDimmingPeriodFactor": 0.5
-}');
+		"colorGradientLengthFactor": 0.051061617376016236,
+		"colors": [
+			{
+				"b": 6,
+				"g": 83,
+				"r": 0
+			},
+			{
+				"b": 117,
+				"g": 18,
+				"r": 52
+			},
+			{
+				"b": 5,
+				"g": 104,
+				"r": 0
+			},
+			{
+				"b": 0,
+				"g": 11,
+				"r": 202
+			},
+			{
+				"b": 0,
+				"g": 203,
+				"r": 241
+			}
+		],
+		"id": 5,
+		"name": "Wildflowers",
+		"patternType": "LEDPattern",
+		"repitionFactor": 0.5717067352681374
+    }');
 
 INSERT INTO LED_PATTERN (JSON) VALUES ('{
-	"name": "chase_animation",
-	"colors": [
-		{
-			"r": 255,
-			"g": 0,
-			"b": 0
-		},
-		{
-			"r": 0,
-			"g": 255,
-			"b": 0
-		},
-		{
-			"r": 0,
-			"g": 0,
-			"b": 255
-		}
-	],
-	"repitionFactor": 0,
-	"colorGradientLengthFactor": 0.3,
-	"patternType": "ChaseLEDPattern",
-	"chaseSpeed": 20,
-	"chaseLengthFactor": 0.2,
-	"chaseGradientLengthFactor": 0.25
-}');
+		"blinkDimmingPeriodFactor": 0.8667690986924655,
+		"blinkSpeed": 0.1581278304936896,
+		"colorGradientLengthFactor": 0.43926870920029754,
+		"colors": [
+			{
+				"b": 223,
+				"g": 202,
+				"r": 38
+			},
+			{
+				"b": 148,
+				"g": 40,
+				"r": 0
+			},
+			{
+				"b": 255,
+				"g": 255,
+				"r": 255
+			},
+			{
+				"b": 197,
+				"g": 107,
+				"r": 35
+			}
+		],
+		"id": 6,
+		"name": "Ice Princess",
+		"patternType": "BlinkLEDPattern",
+		"repitionFactor": 0.857830379469776
+    }');
+
+INSERT INTO LED_PATTERN (JSON) VALUES ('{
+        "chaseGradientLengthFactor": 0.5,
+        "chaseLengthFactor": 0.2077665216998616,
+        "chaseSpeed": 14.949313467041245,
+        "colorGradientLengthFactor": 0.3,
+        "colors": [
+            {
+                "b": 0,
+                "g": 0,
+                "r": 255
+            },
+            {
+                "b": 0,
+                "g": 255,
+                "r": 253
+            },
+            {
+                "b": 0,
+                "g": 255,
+                "r": 2
+            },
+            {
+                "b": 252,
+                "g": 17,
+                "r": 19
+            }
+        ],
+        "id": 2,
+        "name": "Wandering Rainbow",
+        "patternType": "ChaseLEDPattern",
+        "repitionFactor": 0.0
+    }');
+
+INSERT INTO LED_PATTERN (JSON) VALUES ('{
+        "blinkDimmingPeriodFactor": 0.4184565871837002,
+        "blinkSpeed": 2.290888717148455,
+        "colorGradientLengthFactor": 0.3174805489096559,
+        "colors": [
+            {
+                "b": 20,
+                "g": 219,
+                "r": 0
+            },
+            {
+                "b": 0,
+                "g": 85,
+                "r": 10
+            },
+            {
+                "b": 0,
+                "g": 58,
+                "r": 36
+            }
+        ],
+        "id": 8,
+        "name": "Jungle Techno",
+        "patternType": "BlinkLEDPattern",
+        "repitionFactor": 0.3613504173188724
+    }');
+
+INSERT INTO LED_PATTERN (JSON) VALUES ('{
+        "colorGradientLengthFactor": 0.39274187231771635,
+        "colors": [
+            {
+                "b": 177,
+                "g": 0,
+                "r": 7
+            },
+            {
+                "b": 0,
+                "g": 250,
+                "r": 255
+            },
+            {
+                "b": 0,
+                "g": 0,
+                "r": 201
+            },
+            {
+                "b": 9,
+                "g": 192,
+                "r": 0
+            }
+        ],
+        "id": 9,
+        "name": "Party Light Chain",
+        "patternType": "LEDPattern",
+        "repitionFactor": 1.0
+    }');
+
+INSERT INTO LED_PATTERN (JSON) VALUES ('{
+        "chaseForeground": {
+            "b": 0,
+            "g": 0,
+            "r": 0
+        },
+        "chaseGradientLengthFactor": 0.5,
+        "chaseLengthFactor": 0.16548362995520272,
+        "chaseSpeed": 27.201141418510414,
+        "colorGradientLengthFactor": 0.21858536037373535,
+        "colors": [
+            {
+                "b": 0,
+                "g": 76,
+                "r": 143
+            },
+            {
+                "b": 0,
+                "g": 0,
+                "r": 68
+            },
+            {
+                "b": 0,
+                "g": 216,
+                "r": 248
+            },
+            {
+                "b": 0,
+                "g": 34,
+                "r": 175
+            }
+        ],
+        "id": 10,
+        "name": "Campfire",
+        "patternType": "ChaseLEDPattern",
+        "repitionFactor": 0.47860626274280793
+    }');
